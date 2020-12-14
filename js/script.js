bf0335c0b1140e510677f40ef0460585bb6b1f3c
Melvin Sierra <msierrawd@gmail.com>
2020-12-14 11:26:04 -0500

    Adding basic ideas/game rules

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 let scoreToWin = 3; 
+let leftScore = 3; 
+let rightScore = 0; 
+
+function checkCurrentScore() {
+    if(leftScore === scoreToWin) {
+        console.log("Left side won!")
+    }else if (rightScore === scoreToWin) {
+        console.log("Righ side won!")
+    }
+}
+
+function gameRules () {
+    
+}
