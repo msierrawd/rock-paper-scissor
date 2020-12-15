e54bdf87d2742819af0edf114ee0c999a541680f
Melvin Sierra <msierrawd@gmail.com>
2020-12-14 21:11:02 -0500

    Additional game functions were added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-let scoreToWin = 3; 
-let leftScore = 3; 
+let scoreToWin = 1; 
+let leftScore = 0; 
 let rightScore = 0; 
-let leftChoice = " ";
-let rightChoice = " ";
+let leftChoice = "";
+let rightChoice = "";
 
 function checkMatchScore() {
     if(leftScore === scoreToWin) {
@@ -13,39 +13,64 @@ function checkMatchScore() {
 }
 
 
+function submitScoreChange(int) {
+    scoreToWin = int;
+}
+
+
+
 // When player choice buttons are clicked 
 // leftChoice changes string value to what they picked 
 // leftImage change src to match image of string value
-document.getElementById("rockBtn").addEventListener("click", function() {
+document.getElementById("rockLBtn").addEventListener("click", function() {
     leftChoice = "rock";
     document.getElementById("leftImage").src="./images/rockL.png"
   });
-  document.getElementById("scissorBtn").addEventListener("click", function() {
+  document.getElementById("scissorLBtn").addEventListener("click", function() {
     leftChoice = "scissor";
     document.getElementById("leftImage").src="./images/scissorL.png"
   });
-  document.getElementById("paperBtn").addEventListener("click", function() {
+  document.getElementById("paperLBtn").addEventListener("click", function() {
     leftChoice = "paper";
     document.getElementById("leftImage").src="./images/paperL.png"
   });
 
-console.log(leftChoice)
+
+  document.getElementById("rockRBtn").addEventListener("click", function() {
+    rightChoice = "rock";
+    document.getElementById("rightImage").src="./images/rockR.png"
+  });
+  document.getElementById("scissorRBtn").addEventListener("click", function() {
+    rightChoice = "scissor";
+    document.getElementById("rightImage").src="./images/scissorR.png"
+  });
+  document.getElementById("paperRBtn").addEventListener("click", function() {
+    rightChoice = "paper";
+    document.getElementById("rightImage").src="./images/paperR.png"
+  });
 
 // Checks to see if there are any winning or tie-ing situations
 function gameRules () {
     if (leftChoice === rightChoice) {
         console.log("TIE!");
     }else if(leftChoice === "rock" && rightChoice === "paper") {
-        rightScore++
+        rightScore+= 1;
+        document.getElementById("rightScoreDisplay").innerHTML ++ ;
     }else if (leftChoice === "rock" && rightChoice === "scissor") {
-        leftScore++
+        leftScore+= 1;
+        document.getElementById("leftScoreDisplay").innerHTML ++ ;
     }else if (leftChoice === "paper" && rightChoice === "rock") {
-        leftChoice++ 
+        leftChoice+= 1;
+        document.getElementById("leftScoreDisplay").innerHTML ++ ;
     }else if (leftChoice === "paper" && rightChoice === "scissor"){
-        rightChoice++
+        rightChoice+= 1;
+        document.getElementById("rightScoreDisplay").innerHTML ++ ;
     }else if (leftChoice === "scissor" && rightChoice === "rock") {
-        rightChoice++
+        rightChoice+=1;
+        document.getElementById("rightScoreDisplay").innerHTML ++ ;
     }else if (leftChoice === "scissor" && rightChoice === "paper") {
-        leftChoice++ 
+        leftChoice+= 1;
+        document.getElementById("leftScoreDisplay").innerHTML ++ ;
     }
 }
+gameRules();
