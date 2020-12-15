920dbc31a7c1c0f401936e01b51ffbb8b49eab68
Melvin Sierra <msierrawd@gmail.com>
2020-12-14 19:47:51 -0500

    Adding on to game rules

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
 let scoreToWin = 3; 
 let leftScore = 3; 
 let rightScore = 0; 
+let leftChoice = " ";
+let rightChoice = " ";
 
 function checkCurrentScore() {
     if(leftScore === scoreToWin) {
-        console.log("Left side won!")
+        console.log("Left side won!");
     }else if (rightScore === scoreToWin) {
-        console.log("Righ side won!")
+        console.log("Righ side won!");
     }
 }
 
 function gameRules () {
-    
+    if (leftChoice === rightChoice) {
+        console.log("TIE!");
+    }else if(leftChoice === "rock" && rightChoice === "paper") {
+        leftScore++
+    }else if (leftChoice === "rock" && rightChoice === "scissor") {
+        rightScore++
+    }else if (leftChoice === "paper" && rightChoice === "rock") {
+        leftChoice++ 
+    }else if (leftChoice === "paper" && rightChoice === "scissor"){
+        rightChoice++
+    }else if (leftChoice === "scissor" && rightChoice === "rock") {
+        rightChoice++
+    }else if (leftChoice === "scissor" && rightChoice === "paper") {
+        rightChoice++ 
+    }
 }
