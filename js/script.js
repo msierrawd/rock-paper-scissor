e2d8c277c1c3d745f7cc05e2db85e1b8c547ce8c
Melvin Sierra <msierrawd@gmail.com>
2020-12-14 20:14:55 -0500

    Made buttons make changes to leftChoice variable and changes to leftImage

--- js/script.js
+++ js/script.js
@@ -12,6 +12,26 @@ function checkMatchScore() {
     }
 }
 
+
+// When player choice buttons are clicked 
+// leftChoice changes string value to what they picked 
+// leftImage change src to match image of string value
+document.getElementById("rockBtn").addEventListener("click", function() {
+    leftChoice = "rock";
+    document.getElementById("leftImage").src="./images/rockL.png"
+  });
+  document.getElementById("scissorBtn").addEventListener("click", function() {
+    leftChoice = "scissor";
+    document.getElementById("leftImage").src="./images/scissorL.png"
+  });
+  document.getElementById("paperBtn").addEventListener("click", function() {
+    leftChoice = "paper";
+    document.getElementById("leftImage").src="./images/paperL.png"
+  });
+
+console.log(leftChoice)
+
+// Checks to see if there are any winning or tie-ing situations
 function gameRules () {
     if (leftChoice === rightChoice) {
         console.log("TIE!");
